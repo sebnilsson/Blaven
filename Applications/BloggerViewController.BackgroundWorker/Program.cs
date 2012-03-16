943ff07148389a6dd1f7baad36c08d2f91b31b0a
Seb Nilsson <seb.nilsson@gmail.com>
2012-03-16 16:19:16 +0100

    BackgroundWorker now is not asynchronous and error-checking for configuration was added.

--- Applications/BloggerViewController.BackgroundWorker/Program.cs
+++ Applications/BloggerViewController.BackgroundWorker/Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Configuration;
+using System.Diagnostics;
 using System.Net;
 using System.Threading;
 
 namespace BloggerViewController.BackgroundPoller {
     class Program {
         static void Main(string[] args) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             string updateUrl = ConfigurationManager.AppSettings["BloggerViewController.UpdateUrl"];
 
-            var callback = new System.Threading.WaitCallback((url) => {
-                WebRequest request = System.Net.WebRequest.Create(Convert.ToString(url));
-                request.GetResponse();
-            });
-            System.Threading.ThreadPool.QueueUserWorkItem(callback, updateUrl);
-
             int timeout;
-            int.TryParse(ConfigurationManager.AppSettings["BloggerViewController..AppTimout"], out timeout);
+            int.TryParse(ConfigurationManager.AppSettings["BloggerViewController.AppTimout"], out timeout);
 
-            Thread.Sleep(timeout * 60 * 1000);
+            if(string.IsNullOrWhiteSpace(updateUrl)) {
+                throw new ConfigurationErrorsException("The configuration-value for 'BloggerViewController.UpdateUrl' was empty.");
+            }
+
+            if(timeout <= 0) {
+                throw new ConfigurationErrorsException("The configuration-value for 'BloggerViewController.AppTimout' was empty.");
+            }
+            
+            WebRequest request = System.Net.WebRequest.Create(Convert.ToString(updateUrl));
+            request.GetResponse();
+            
+            stopwatch.Stop();
+
+            int elapsed = (int)stopwatch.ElapsedMilliseconds;
+            int timeOut = Math.Max(0, timeout * 60 * 1000 - elapsed);
+
+            Thread.Sleep(timeOut);
         }
     }
 }
