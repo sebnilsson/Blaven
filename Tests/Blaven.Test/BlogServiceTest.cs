26071b1c43c865d585b58c36536f02d4c65231ec
sebnilsson <seb.nilsson@gmail.com>
2012-06-28 15:06:21 +0200

    Marked Date from Blogger correctly as UTC.

--- Tests/Blaven.Test/BlogServiceTest.cs
+++ Tests/Blaven.Test/BlogServiceTest.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Raven.Client.Embedded;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Blaven.Test.Integration {
     [TestClass]
@@ -19,6 +24,19 @@ namespace Blaven.Test.Integration {
         [ClassCleanup()]
         public static void ClassCleanup() {
 
+        }
+
+        [TestMethod]
+        public void ctor_UsingMultipleCalls_ShouldOnlyUpdateOnce() {
+            var resultsList = new ConcurrentBag<bool>();
+
+            Parallel.For(0, 5, (i) => {
+                var blogStore = DocumentStoreTestHelper.GetEmbeddableDocumentStore("ctor_UsingMultipleCalls_ShouldOnlyUpdateOnce_" + i);
+                var blogService = BlogServiceTestHelper.GetBlogService(blogStore, "TEST_" + i, "buzz");
+            });
+
+            int trueCount = resultsList.Count(x => x == true);
+            Assert.AreEqual<int>(1, trueCount);
         }
 
         //[TestMethod]
