b88240a87e732963aeb6dfb9be5b3284d3c95b59
sebnilsson <seb.nilsson@gmail.com>
2012-07-03 15:07:18 +0200

    Fixed tests depending on indexes, but not using BlogService.

--- Tests/Blaven.Test/DocumentStoreTestHelper.cs
+++ Tests/Blaven.Test/DocumentStoreTestHelper.cs
@@ -5,12 +5,12 @@ using Raven.Client.Indexes;
 
 namespace Blaven.Test {
     public static class DocumentStoreTestHelper {
-        public static EmbeddableDocumentStore GetEmbeddableDocumentStore() {
+        public static EmbeddableDocumentStore GetEmbeddableDocumentStore(bool createIndexes = false) {
             string randomPath = new Random().Next().ToString();
-            return GetEmbeddableDocumentStore(randomPath);
+            return GetEmbeddableDocumentStore(randomPath, createIndexes);
         }
 
-        public static EmbeddableDocumentStore GetEmbeddableDocumentStore(string path) {
+        public static EmbeddableDocumentStore GetEmbeddableDocumentStore(string path, bool createIndexes = false) {
             var documentStore = new EmbeddableDocumentStore {
                 Configuration = {
                     DataDirectory = path,
@@ -21,6 +21,11 @@ namespace Blaven.Test {
                 RunInMemory = true,
             };
             documentStore.Initialize();
+
+            if(createIndexes) {
+                Raven.Client.Indexes.IndexCreation.CreateIndexes(
+                    typeof(Blaven.RavenDb.Indexes.BlogPostsOrderedByCreated).Assembly, documentStore);
+            }
 
             return documentStore;
         }
