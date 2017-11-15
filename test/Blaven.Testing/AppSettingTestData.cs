0d777f81f6acbe393bbef390073302ecdc82a35f
Seb Nilsson <seb.nilsson@gmail.com>
2017-11-15 17:15:03 +0100

    Removed help-functionality for configuration

--- test/Blaven.Testing/AppSettingTestData.cs
+++ test/Blaven.Testing/AppSettingTestData.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Blaven.BlogSources.Testing;
 
 namespace Blaven.Testing
@@ -9,45 +8,25 @@ namespace Blaven.Testing
         public const string AppSettingsTestPassword = "TestPassword";
         public const string AppSettingsTestUsername = "TestUsername";
 
-        public static IDictionary<string, string> CreateDictionary()
+        public static IEnumerable<KeyValuePair<string, string>> CreateDictionary()
         {
-            var appSettings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
-                              {
-                                  {
-                                      $"Blaven.BlogSources.{nameof(FakeBlogSource)}.Username",
-                                      AppSettingsTestUsername
-                                  },
-                                  {
-                                      $"Blaven.BlogSources.{nameof(FakeBlogSource)}.Password",
-                                      AppSettingsTestPassword
-                                  },
-                                  {
-                                      "Blaven.Blogs.BlogKey1.Id",
-                                      "BlogKey1Id"
-                                  },
-                                  {
-                                      "Blaven.Blogs.BlogKey1.Name",
-                                      "BlogKey1Name"
-                                  },
-                                  {
-                                      "Blaven.Blogs.BlogKey2.Id",
-                                      "BlogKey2Id"
-                                  },
-                                  {
-                                      "Blaven.Blogs.BlogKey2.Name",
-                                      "BlogKey2Name"
-                                  },
-                                  {
-                                      "Blaven.Blogs.BlogKey3",
-                                      string.Empty
-                                  },
-                                  {
-                                      "Blaven.BlogSources.Blogger.ApiKey",
-                                      "TestApiKey"
-                                  }
-                              };
+            yield return new KeyValuePair<string, string>($"Blaven.BlogSources.{nameof(FakeBlogSource)}.Username",
+                AppSettingsTestUsername);
 
-            return appSettings;
+            yield return new KeyValuePair<string, string>($"Blaven.BlogSources.{nameof(FakeBlogSource)}.Password",
+                AppSettingsTestPassword);
+
+            yield return new KeyValuePair<string, string>("Blaven.Blogs.BlogKey1.Id", "BlogKey1Id");
+
+            yield return new KeyValuePair<string, string>("Blaven.Blogs.BlogKey1.Name", "BlogKey1Name");
+
+            yield return new KeyValuePair<string, string>("Blaven.Blogs.BlogKey2.Id", "BlogKey2Id");
+
+            yield return new KeyValuePair<string, string>("Blaven.Blogs.BlogKey2.Name", "BlogKey2Name");
+
+            yield return new KeyValuePair<string, string>("Blaven.Blogs.BlogKey3", string.Empty);
+
+            yield return new KeyValuePair<string, string>("Blaven.BlogSources.Blogger.ApiKey", "TestApiKey");
         }
     }
 }
